2805ee49fb2eacd83e5ea75c854832cbdfb2d6b9
Tahseen Kazi <myronintro@gmail.com>
2018-08-30 21:48:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your functions and code here
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+  return kittens;
+}
+
+function appendKitten(name){
+  return [...kittens, name];
+}
+
+function prependKitten(name){
+  return [name, ... kittens]
+}
+
+function removeLastKitten(){
+  kittens.slice();
+}
+
+function removeFirstKitten(){
+  kittens.slice(1);
+}
+
